678fb54ea3c8c60df55c414db55fdaf45131f37d
PappuPanaut <170117104+PappuPanaut@users.noreply.github.com>
2024-06-19 13:18:28 +0000

    Rename build to pull

--- src/range_op/range_update_point_query/segment_tree_lazy.rs
+++ src/range_op/range_update_point_query/segment_tree_lazy.rs
@@ -68,7 +68,7 @@ impl SegmentTree {
         self.build(r0);
     }
 
-    fn build(&mut self, mut i: usize) {
+    fn pull(&mut self, mut i: usize) {
         while i > 1 {
             i /= 2;
             self.t[i] = self.d[i];
