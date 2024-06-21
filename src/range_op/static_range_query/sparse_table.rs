d405a3347a7b9bbbd631dd0b4c156c7723a8f6ec
PappuPanaut <170117104+PappuPanaut@users.noreply.github.com>
2024-06-21 11:50:15 +0000

    Create sparse_table.rs

---
+++ src/range_op/static_range_query/sparse_table.rs
@@ -0,0 +1,24 @@
+struct SparseTable {
+    t: Vec<Vec<u32>>,
+}
+
+impl SparseTable {
+    fn with_slice(s: &[u32]) -> Self {
+        let mut t = vec![vec![0; s.len()]; 1 + s.len().ilog2() as usize];
+        t[0] = s.to_vec();
+
+        for b in 1..t.len() {
+            for i in 0..=s.len() - (1 << b) {
+                t[b][i] = t[b - 1][i].min(t[b - 1][i + (1 << (b - 1))]);
+            }
+        }
+
+        Self { t }
+    }
+
+    fn min_range(&self, l: usize, r: usize) -> u32 {
+        let b = (r - l + 1).ilog2() as usize;
+
+        self.t[b][l].min(self.t[b][r + 1 - (1 << b)])
+    }
+}
