dd21074fb5dda2d8f2b7e1ecb3c825c69cc6b3be
PappuPanaut <170117104+PappuPanaut@users.noreply.github.com>
2024-09-22 10:40:54 +0000

    Update trie.rs

--- src/trie.rs
+++ src/trie.rs
@@ -1,45 +1,34 @@
-#[derive(Default, Clone)]
-struct Node {
-    next: [u32; 2],
-}
-
 struct Trie {
-    t: Vec<Node>,
+    t: Vec<[usize; 2]>,
 }
 
 impl Trie {
-    fn new() -> Self {
-        Self {
-            t: vec![Node::default(); 1],
-        }
-    }
+    fn new() -> Self { Self { t: vec![[0, 0]] } }
 
-    fn insert(&mut self, x: u64) {
+    fn add(&mut self, x: usize) {
         let mut v = 0;
-
-        for xb in (0..41).rev().map(|b| (x >> b) as usize & 1) {
-            if self.t[v].next[xb] == 0 {
-                self.t[v].next[xb] = self.t.len() as _;
-                self.t.push(Node::default());
+        for xb in (0..41).rev().map(|b| (x >> b) & 1) {
+            if self.t[v][xb] == 0 {
+                self.t[v][xb] = self.t.len();
+                self.t.push([0, 0]);
             }
 
-            v = self.t[v].next[xb] as _;
+            v = self.t[v][xb];
         }
     }
 
-    fn max_xor_with(&self, x: u64) -> u64 {
+    fn max_xor(&self, x: usize) -> u64 {
         let mut res = 0;
 
         let mut v = 0;
-        for xb in (0..41).rev().map(|b| (x >> b) as usize & 1) {
+        for xb in (0..41).rev().map(|b| (x >> b) & 1) {
             res *= 2;
 
-            match self.t[v].next[1 ^ xb] > 0 {
-                true => {
-                    res += 1;
-                    v = self.t[v].next[1 ^ xb] as _;
-                }
-                false => v = self.t[v].next[xb] as _,
+            if self.t[v][1 ^ xb] == 0 {
+                v = self.t[v][xb]
+            } else {
+                res += 1;
+                v = self.t[v][1 ^ xb];
             }
         }
 
