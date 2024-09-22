54a5dd308119e81c1049feb7302d6ac2808f699e
PappuPanaut <170117104+PappuPanaut@users.noreply.github.com>
2024-09-22 10:24:02 +0000

    Update scanner.rs

--- src/scanner.rs
+++ src/scanner.rs
@@ -4,19 +4,13 @@ struct Scanner {
 }
 
 impl Scanner {
-    /// Returns white-space separated tokens one at a time
-    ///
-    /// # Panics
-    ///
-    /// Panics if there's an I/O error or if the token can't be parsed as T
     fn next<T: std::str::FromStr>(&mut self) -> T {
         if let Some(token) = self.buf.pop() {
-            return token.parse().ok().expect("Failed parse");
+            return token.parse().ok().expect("Parse fail");
         }
 
         let mut ln = String::new();
-        std::io::stdin().read_line(&mut ln).expect("Failed read");
-
+        std::io::stdin().read_line(&mut ln).expect("Read fail");
         self.buf = ln.split_whitespace().rev().map(String::from).collect();
 
         self.next()
