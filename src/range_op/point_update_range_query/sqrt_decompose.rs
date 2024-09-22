44632d4f833693fccd0457f48c248cbb8b6c55a3
PappuPanaut <170117104+PappuPanaut@users.noreply.github.com>
2024-09-22 10:51:31 +0000

    Update sqrt_decompose.rs

--- src/range_op/point_update_range_query/sqrt_decompose.rs
+++ src/range_op/point_update_range_query/sqrt_decompose.rs
@@ -5,17 +5,14 @@ struct SqrtDecompose {
 
 impl SqrtDecompose {
     fn with_len(n: usize) -> Self {
-        Self {
-            blocks: vec![0; 450],
-            arr: vec![0; n],
-        }
+        Self { blocks: vec![0; 450],
+               arr: vec![0; n] }
     }
 
     fn prefix_sum(&self, r: isize) -> i64 {
         (0..r / 450).map(|i| self.blocks[i as usize]).sum::<i64>()
-            + ((r / 450) * 450..=r)
-                .map(|i| self.arr[i as usize])
-                .sum::<i64>()
+        + ((r / 450) * 450..=r).map(|i| self.arr[i as usize])
+                               .sum::<i64>()
     }
 
     fn range_sum(&self, range: std::ops::RangeInclusive<isize>) -> i64 {
@@ -27,7 +24,5 @@ impl SqrtDecompose {
         self.blocks[i / 450] += da;
     }
 
-    fn set(&mut self, i: usize, da: i64) {
-        self.increment(i, da - self.arr[i]);
-    }
+    fn set(&mut self, i: usize, da: i64) { self.increment(i, da - self.arr[i]); }
 }
